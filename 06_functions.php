5e6afabb4076e3731ef4bb33e858dad308e20b26
Nafi <saejim91@gmail.com>
2023-01-31 16:44:59 +0600

    functions added

---
+++ 06_functions.php
@@ -0,0 +1,36 @@
+<?php
+//REMEMBER: global variable and fucntional variables are scoped. you can not access to each other. only way you can access to a global variable is with GLOBAL keyword.
+
+$y = 23;
+
+    function registerUser($email){
+        // global $y;
+        // echo $y;
+
+        // $functionVar = 'something';
+        echo "$email registered.";
+    }
+
+    // echo $functionVar;  //DOES NOT HAVE ACCESS
+
+    // registerUser('some@somemail.com');
+
+
+
+    // RETURN VALUE
+
+
+    function sum($n1 = 4, $n2 = 4){
+        return $n1 + $n2;
+    }
+
+// echo sum(5, 5);
+// echo sum();
+
+
+//-------------------- ARROW
+
+$multiply = fn($n1, $n2) => $n1 * $n2;
+
+
+echo $multiply(9,9);
