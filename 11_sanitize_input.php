30dbc4e6fd09e32f6b35144111e957dfb2af3fd5
Nafi <saejim91@gmail.com>
2023-02-01 19:27:38 +0600

    glimpse of sanitization

---
+++ 11_sanitize_input.php
@@ -0,0 +1,47 @@
+<?php
+
+//To get pass any hacky tacky
+
+//*----- htmlspecialchars
+
+// if(isset($_POST['submit'])){
+
+//     $name = htmlspecialchars($_POST['name']);
+//     $age= htmlspecialchars($_POST['age']);
+//     // echo $_POST['name'];
+//     // echo $_POST['age'];
+//     echo $name;
+//     echo $age;
+// }
+
+
+//*----- or filter_input
+if(isset($_POST['submit'])){
+
+    $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
+    $age= filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
+    // echo $_POST['name'];
+    // echo $_POST['age'];
+    echo $name;
+    echo $age;
+}
+
+
+?>
+
+
+
+<!-- By default it is GET but use POST anytime you want to submit any value -->
+
+<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
+<div>
+    <label for="name">Name:</label>
+    <input type="text" name="name">
+</div>
+<div>
+    <label for="age">age</label>
+    <input type="text" name="age">
+</div>
+
+<input type="submit" value="submit" name="submit">
+</form>
