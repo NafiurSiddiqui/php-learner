2a7ba03c3f9dbf964aa98f3936ff3ac9b6cfba8b
Nafi <saejim91@gmail.com>
2023-02-05 18:23:42 +0600

    file handling updated

--- 14_file_handling.php
+++ 14_file_handling.php
@@ -2,12 +2,15 @@
 
 //File handling: abiltiy to RW files on server.
 
-$file = 'extras/users2.txt';
+/**
+ * @mode - A = append, R = Read, W = write
+ */
+
+$file = 'extras/existential.txt';
 
 if(file_exists($file)){
     //Additional integer is the number of Bytes of the file.
     // echo readfile($file);
-
     $handle = fopen($file, 'r') ;
 
     $contents = fread($handle, filesize($file));
@@ -19,9 +22,15 @@ if(file_exists($file)){
     //if the file does not exist.craetes one.
 
     $handle = fopen($file, 'w');
-    $contents = 'File created by Php'. PHP_EOL. 'And more on the way!';//EOL = Line break
+    $contents = 'File created by Php'. PHP_EOL. 'And more on the way! ';//EOL = Line break
 
     fwrite($handle, $contents);
+
+    $anotherContent = 'Non existensial existence';
+    fwrite($handle, $anotherContent);
     fclose($handle);
 
 }
+
+
+// * Always close the file afterwards.
