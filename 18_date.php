f9c5ca71694a4cf811d81d68ed24eca4e431c947
Nafi <saejim91@gmail.com>
2023-02-05 17:34:30 +0600

    local time added

--- 18_date.php
+++ 18_date.php
@@ -1,8 +1,27 @@
 <?php
 
-    $time = date("d.m.Y <br>");
-    $dweek = date('l');
-  
+    //PHP DATE() returns current time of the server.
+
+    $calendar = date("d.m.Y "); //Format can be anything
+   
+    $time = date("H:i:s "); //Hour, minute, second
+
+    $dweek = date('l ');//Day of the week.
+
+    echo $calendar;
     echo $time;
+    echo $dweek;
+
+    //GETTING LOCAL TIME
+
+    date_default_timezone_set("Asia/Dhaka");
+
+     $BdTime = date("H:i:s ");
+
+    echo $BdTime;
+    
+  
+
+
 
 ?>
