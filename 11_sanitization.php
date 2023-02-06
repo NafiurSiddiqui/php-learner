710d3fdcabd2b7175fd74d52e0c70a3edca49889
Nafi <saejim91@gmail.com>
2023-02-06 18:08:30 +0600

    sanitization added

--- 11_sanitization.php
+++ 11_sanitization.php
@@ -15,13 +15,23 @@
 // }
 
 
-//*----- or filter_input
+//*----- or  use PHP filers like, filter_input and more. 
 if(isset($_POST['submit'])){
 
     $name = filter_input(INPUT_POST, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
     $age= filter_input(INPUT_POST, 'age', FILTER_SANITIZE_SPECIAL_CHARS);
-    // echo $_POST['name'];
-    // echo $_POST['age'];
+
+    //we can also sanitize variables by htmlspecialchars, previously filter_Var was used.
+
+    $someText = "<h1>Some text</h1>";
+    // $textSanitized = filter_var($someText, FILTER_SANITIZE_STRING);
+    $textSanitized = htmlspecialchars($someText);
+
+
+
+
+
+    echo $textSanitized;
     echo $name;
     echo $age;
 }
@@ -38,10 +48,11 @@ if(isset($_POST['submit'])){
     <label for="name">Name:</label>
     <input type="text" name="name">
 </div>
+<br>
 <div>
     <label for="age">age</label>
     <input type="text" name="age">
 </div>
-
+<br>
 <input type="submit" value="submit" name="submit">
 </form>
